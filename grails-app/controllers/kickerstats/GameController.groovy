f128c6fded168deadeabdc0f26b4b41ae5e63ef5
rbobin <rbobinx@gmail.com>
2015-07-27 00:27:37 +0200

    refactored game controller

--- grails-app/controllers/kickerstats/GameController.groovy
+++ grails-app/controllers/kickerstats/GameController.groovy
@@ -1,6 +1,39 @@
 package kickerstats
 
-class GameController {
+class GameController extends BaseController {
 
-    static scaffold = true
+    def createGame(GameCommand gameCommand) {
+        Game game = gameCommand.game
+        if (game.save()) {
+            renderSuccess(game: game)
+        } else {
+            renderValidationErrors(game)
+        }
+    }
+
+    def deleteGame(Game game) {
+        if (game) {
+            game.delete()
+            renderSuccess()
+        } else {
+            renderNotFound("game")
+        }
+    }
+}
+
+class GameCommand {
+
+    Team winners
+    Team losers
+    Integer winnersScore
+    Integer losersScore
+
+    def getGame() {
+        new Game(
+                winners: winners,
+                losers: losers,
+                winnersScore: winnersScore,
+                losersScore: losersScore
+        )
+    }
 }
--- test/unit/kickerstats/GameControllerSpec.groovy
+++ test/unit/kickerstats/GameControllerSpec.groovy
@@ -1,20 +1,81 @@
 package kickerstats
 
+import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
+import org.apache.commons.lang.RandomStringUtils
+import org.apache.commons.lang.math.RandomUtils
 import spock.lang.Specification
 
-/**
- * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
- */
 @TestFor(GameController)
+@Mock([Team, Player, Game, Challenge])
+@SuppressWarnings("GroovyAssignabilityCheck")
 class GameControllerSpec extends Specification {
 
-    def setup() {
+    def "test createGame"() {
+        given:
+        mockCommandObject GameCommand
+        new Challenge(current: true).save()
+
+        when:
+        controller.createGame()
+        then:
+        400 == response.status
+        !response.json.success
+        4 == response.json.errors.size()
+        !response.json.game
+        0 == Game.list().size()
+
+        when:
+        params.losersScore = 0
+        params.winnersScore = Game.MAX_SCORE
+        params.losers = [id: team.id]
+        params.winners = [id: team.id]
+        response.reset()
+        controller.createGame()
+        then:
+        200 == response.status
+        response.json.success
+        !response.json.errors
+        0 == response.json.game.losersScore
+        1 == Game.list().size()
     }
 
-    def cleanup() {
+    def "test deleteGame"() {
+        given:
+        new Challenge(current: true).save()
+        def game = new Game(
+                losersScore: 0,
+                winnersScore: Game.MAX_SCORE,
+                losers: team,
+                winners: team
+        ).save()
+
+        when:
+        controller.deleteGame()
+        then:
+        404 == response.status
+        !response.json.success
+        ['game not found'] == response.json.errors
+        1 == Game.list().size()
+
+        when:
+        params.id = game.id
+        response.reset()
+        controller.deleteGame()
+        then:
+        200 == response.status
+        response.json.success
     }
 
-    void "test something"() {
+    def getTeam() {
+        new Team(offence: player, defence: player, title: 'Title').save()
+    }
+
+    def getPlayer() {
+        def player = new Player()
+        player.firstname = "${RandomStringUtils.randomAlphabetic(5)}"
+        player.lastname = "${RandomStringUtils.randomAlphabetic(5)}"
+        player.id = RandomUtils.nextInt()
+        return player.save()
     }
 }
